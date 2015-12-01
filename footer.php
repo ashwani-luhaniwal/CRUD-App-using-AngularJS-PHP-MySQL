3a425aadadc74fe144f8be7f77a21ea7fbfb2146
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2015-12-01 11:43:52 +0530

    Ashwani: removed unnecessary files from the project.

--- footer.php
+++ footer.php
@@ -2,33 +2,6 @@
 	<footer role="contentinfo">
 		
 	</footer>
-	
-	<script type="text/javascript" src="js/custom-script.js"></script>
-	<script type="text/javascript">
-		$(function(){
-			var shrinkHeader = 200;
-			$(window).scroll(function(){
-				var scroll = getCurrentScroll();
-				if ( scroll >= shrinkHeader ) {
-					$('header').addClass('scrolled');
-				}
-				else {
-					$('header').removeClass('scrolled');
-				}
-			});
-			function getCurrentScroll() {
-				return window.pageYOffset || document.documentElement.scrollTop;
-			}
-		});
-		$(document).ready(function() {
-			$('#quick-agreebtn').click(function() {
-				$('#overlay').css('display', 'block');
-			});
-			$('#full-agreebtn').click(function() {
-				$('#overlay').css('display', 'block');
-			});
-		});
-	</script>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,16 +6,12 @@
 	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/chosen.css">
 	<link rel="stylesheet" type="text/css" href="angular-datepicker.css">
 
 	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="js/jquery.validate.min.js"></script>
-	<script type="text/javascript" src="js/chosen.jquery.js"></script>
 
 	<script type="text/javascript" src="js/angular.min.js"></script>
 	<script type="text/javascript" src="js/angular-initial-value.min.js"></script>
